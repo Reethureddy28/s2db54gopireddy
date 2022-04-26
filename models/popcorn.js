66a5addbe59e2a32d62daf3239aef5ab4de3d52b
Reethureddy28 <reethureddy2000@gmail.com>
2022-04-26 00:43:09 -0500

    good message

--- models/popcorn.js
+++ models/popcorn.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose") 
 const popcornSchema = mongoose.Schema({ 
- popcorn_flavour: String, 
- popcorn_quantity: Number, 
+ popcorn_flavour: {
+     type: String,
+     required: true
+ },
+ popcorn_quantity: {
+     type: Number,
+     min: 2,
+     max: 1111
+ }, 
  popcorn_cost: Number 
 }) 
  
--- routes/popcorn.js
+++ routes/popcorn.js
@@ -1,6 +1,14 @@
 var express = require('express'); 
 const popcorn_controlers= require('../controllers/popcorn'); 
 var router = express.Router(); 
+
+const secured = (req, res, next) => {
+    if (req.user) {
+      return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+  }
  
 /* GET popcorn */ 
 router.get('/', popcorn_controlers.popcorn_view_all_Page ); 
@@ -12,13 +20,13 @@ router.get('/popcorn/:id', popcorn_controlers.popcorn_detail);
 router.get('/detail', popcorn_controlers.popcorn_view_one_Page); 
 
 /* GET create popcorn page */ 
-router.get('/create', popcorn_controlers.popcorn_create_Page); 
+router.get('/create',secured, popcorn_controlers.popcorn_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', popcorn_controlers.popcorn_update_Page); 
+router.get('/update',secured, popcorn_controlers.popcorn_update_Page); 
 
 /* GET delete popcorn page */ 
-router.get('/delete', popcorn_controlers.popcorn_delete_Page); 
+router.get('/delete',secured, popcorn_controlers.popcorn_delete_Page); 
  
 
 module.exports = router; 
